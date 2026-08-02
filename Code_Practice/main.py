eb999e4f50da6d8ca83e6d812b927d812e78426b
Lokendra <thelokendrakushwaha@gmail.com>
2026-08-02 20:42:15 +0530

    Created a calculator using arithmetic operators.

---
+++ Code_Practice/main.py
@@ -0,0 +1,12 @@
+# My First Python Program
+print("Lokendra Kushwaha is Great.")
+
+# Calculation on two numbers using arithmetic operators
+x = 10
+y = 6
+print(x, "+", y, "=", x + y)
+print(x, "-", y, "=", x - y)
+print(x, "x", y, "=", x * y)
+print(x, "/", y, "=", x / y)
+print(x, "//", y, "=", x // y, "(Floor Division)")
+print(x, "%", y, "=", x % y, "(Modulus)")

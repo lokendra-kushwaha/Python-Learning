480b0269ce2093e184ca550539f6a3268cf4197a
Lokendra <thelokendrakushwaha@gmail.com>
2026-08-05 19:56:56 +0530

    A program that removes suffix and prefix from a given string without using in-built function removesuffix() and removeprefix().

--- Python_Logics/remove_suffix_prefix_without_function.py
+++ Python_Logics/remove_suffix_prefix_without_function.py
@@ -1,3 +1,9 @@
+"""
+A program that decodes python in-built function removeprefix() function.
+Input: A target substring (prefix) that user wants to remove from the starting of the string.
+Execution: Checks if the string starts with the exact target substring using positive indexing and slicing. If a match is found, it slices off the prefix.
+Output: Return the modified string (or the original string if the prefix is not found).
+"""
 string = 'lokendra'
 prefix_remove = 'lo'
 
@@ -9,6 +15,12 @@ else:
 
 print(result)
 
+"""
+A program that decodes python in-built function removesuffix() function.
+Input: A target substring (suffix) that user wants to remove from the end of the string.
+Execution: Checks if the string ends with the exact target substring using negative indexing and slicing. If a match is found, it slices off the suffix.
+Output: Return the modified string (or the original string if the suffix is not found).
+"""
 string = 'lokendra'
 suffix_remove = 'dra'
 
